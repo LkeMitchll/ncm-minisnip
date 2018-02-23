6ee20b3032adacf50d067ae6255f85bc40d55192
LkeMitchll <mail@luke.fail>
2018-02-23 10:10:00 +0000

    Refactor and lint

--- pythonx/cm_sources/minisnip.py
+++ pythonx/cm_sources/minisnip.py
@@ -1,22 +1,35 @@
-# -*- coding: utf-8 -*-
-
+""" An ncm source for vim-minisnip """
+from os import listdir, path
 from cm import register_source, Base, getLogger
-import neovim
-import os
-register_source(name='vim-minisnip',
-        abbreviation='minisnip',
-        priority=9)
 
-logger = getLogger(__name__)
+register_source(
+    name='vim-minisnip',
+    abbreviation='minisnip',
+    priority=9)
+
+LOGGER = getLogger(__name__)
 
 class Source(Base):
+    """ The primary class: fetches, processes and returns snippets """
+
     def __init__(self, nvim):
         self.nvim = nvim
         self.minisnip_dir = nvim.eval('get(g:, "minisnip_dir")')
-        self.snippets = os.listdir(os.path.expanduser(self.minisnip_dir))
+        self.snippets = listdir(path.expanduser(self.minisnip_dir))
+        LOGGER.info('minisnip snippets: %s', self.snippets)
 
-    def cm_refresh(self,info,ctx):
-        filetypes = ctx['filetype'].split('.')
-        filetype = ['_' + filetype + '_' for filetype in filetypes]
-        cleaned = [snippet.split(filetype[0])[1] for snippet in self.snippets if filetype[0] in snippet]
-        self.complete(info, ctx, ctx['startcol'], cleaned)
+    def process_matches(self, filetypes):
+        """ Process and return snippet names"""
+        matches = []
+        for filetype in filetypes:
+            for snippet in self.snippets:
+                if filetype in snippet:
+                    matches.append(snippet.split(filetype)[1])
+        LOGGER.info('minisnip matches: %s', matches)
+        return matches
+
+    def cm_refresh(self, info, ctx):
+        """ Refresh NCM with matches """
+        split_filetypes = ctx['filetype'].split('.')
+        filetypes = ['_' + filetype + '_' for filetype in split_filetypes]
+        self.complete(info, ctx, ctx['startcol'], self.process_matches(filetypes))
